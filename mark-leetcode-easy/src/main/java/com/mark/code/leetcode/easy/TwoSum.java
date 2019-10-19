2bd1ad5b0c1730202abd3387550da25039063ad3
worldisnoposition <1642984594@qq.com>
2019-10-19 21:51:07 +0800

    初步搭建项目

---
+++ mark-leetcode-easy/src/main/java/com/mark/code/leetcode/easy/TwoSum.java
@@ -0,0 +1,60 @@
+package com.mark.code.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 第1题
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * <p>
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 从数组里找到2个相加等于target的数
+ *
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            int temp = target - nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == temp) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 更优化的做法，把参数放到map里，那么只需一次循环
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum_1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            Integer j = map.get(temp);
+            if (j != null) {
+                return new int[]{j, i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
