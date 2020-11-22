0d441085c2f7fe8a182527307d8472ff5ec78aff
worldisnoposition <1642984594@qq.com>
2020-11-22 20:25:30 +0800

    MappedByteBuffer 进程间通信 共享内存

---
+++ mark-test/src/main/java/com/hebo/ipc/mbb/MbbConsumer.java
@@ -0,0 +1,20 @@
+package com.hebo.ipc.mbb;
+
+import sun.misc.Contended;
+
+import java.io.IOException;
+
+public class MbbConsumer {
+    public static void main(String[] args) throws IOException {
+        MappedByteBufferUtil mappedByteBufferUtil = new MappedByteBufferUtil();
+        mappedByteBufferUtil.read();
+
+
+
+    }
+
+}
+@Contended
+ class VolatileLong {
+    public volatile long value = 0L;
+}
