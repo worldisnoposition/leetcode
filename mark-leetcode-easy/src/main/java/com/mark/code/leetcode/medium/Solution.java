ebcdff44639d36cbb359e9ddc9f52db039a90217
worldisnoposition <1642984594@qq.com>
2020-12-21 23:57:18 +0800

    leetcode中级题 IP解析

--- mark-leetcode-easy/src/main/java/com/mark/code/leetcode/medium/Solution.java
+++ mark-leetcode-easy/src/main/java/com/mark/code/leetcode/medium/Solution.java
@@ -2,6 +2,9 @@ package com.mark.code.leetcode.medium;
 
 import com.mark.code.leetcode.common.ListNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -12,6 +15,7 @@ public class Solution {
         first.next.next.next.next = new ListNode(5);
         ListNode result = solution.reverseBetween(first, 2, 4);
         System.out.println(result);
+        System.out.println(solution.restoreIpAddresses("25525511135"));
     }
 
     public ListNode reverseBetween(ListNode head, int m, int n) {
@@ -21,7 +25,7 @@ public class Solution {
         ListNode firstNode = new ListNode(-1);
         ListNode last = first;
         ListNode startFirst = null;
-        int i=0;
+        int i = 0;
         boolean start = true;
         while (head != null) {
             i++;
@@ -50,5 +54,26 @@ public class Solution {
 
         }
         return first.next;
+    }
+
+    List<String> result = new ArrayList<>();
+
+    public List<String> restoreIpAddresses(String s) {
+        restoreIpAddresses(s, 0, new StringBuilder(), 4);
+        return result;
+    }
+
+    public void restoreIpAddresses(String s, int i, StringBuilder sb, int count) {
+        if (i < s.length() && count <= 4 && count >= 0) {
+            restoreIpAddresses(s, i + 1, new StringBuilder(sb).append(s.charAt(i)).append("."), count - 1);
+            if (s.charAt(i) != '0' && (i + 1 < s.length())) {
+                restoreIpAddresses(s, i + 2, new StringBuilder(sb).append(s.substring(i, i + 2)).append("."), count - 1);
+            }
+            if ((i + 2 < s.length()) && ((s.charAt(i) == '1') || (s.charAt(i) == '2' && s.charAt(i + 1) <= '4') || ((s.charAt(i) == '2' && s.charAt(i + 1) == '5' && s.charAt(i + 2) <= '5')))) {
+                restoreIpAddresses(s, i + 3, new StringBuilder(sb).append(s.substring(i, i + 3)).append("."), count - 1);
+            }
+        } else if (i == s.length() && count == 0) {
+            result.add(sb.substring(0, sb.length() - 1));
+        }
     }
 }
