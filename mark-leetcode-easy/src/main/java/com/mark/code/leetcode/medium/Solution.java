d12d351473a1c45911a1eedc88f512a29bddd727
worldisnoposition <1642984594@qq.com>
2020-12-29 23:24:24 +0800

    中级题，共有多少种二叉树

--- mark-leetcode-easy/src/main/java/com/mark/code/leetcode/medium/Solution.java
+++ mark-leetcode-easy/src/main/java/com/mark/code/leetcode/medium/Solution.java
@@ -115,4 +115,16 @@ public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         return false;
     }
+
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j < i + 1; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
 }
